526e1348c5f01cb36efe84db3db2517c6ef3df85
Panji Ragil Wibisono <40693039+panji1700018094@users.noreply.github.com>
2019-06-18 20:50:09 +0700

    18 June 2019
    Perbaikan v2

--- transaction/form.php
+++ transaction/form.php
@@ -31,9 +31,9 @@
                       Categories
                       </a>
                   <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <a class="dropdown-item" href="./categories/alatkemah.php">Alat Kemah</a>
-                      <a class="dropdown-item" href="./categories/alathiking.php">Alat Hiking</a>
-                      <a class="dropdown-item" href="./categories/pakaiantravelling.php">Pakaian Travelling</a>
+                      <a class="dropdown-item" href="../categories/alatkemah.php">Alat Kemah</a>
+                      <a class="dropdown-item" href="../categories/alathiking.php">Alat Hiking</a>
+                      <a class="dropdown-item" href="../categories/pakaiantravelling.php">Pakaian Travelling</a>
                   </div>
                   </li>
                   <li class="nav-item">
@@ -42,7 +42,7 @@
               </ul>
               <!-- copas ini pak -->
               <a class="nav-link my-2 my-lg-0 ml-5" href="login.php">Sign in for Admin</a>
-              <form class="form-inline my-2 my-lg-0" method="GET" action="pencarian.php">
+              <form class="form-inline my-2 my-lg-0" method="GET" action="../pencarian.php">
                   <input class="form-control mr-sm-2" type="text" name="search" placeholder="Search" aria-label="Search" onkeyup="this.value = this.value.toLowerCase();"> 
           <!--      <button class="btn btn-outline-success my-2 my-sm-0" name="search" type="submit">Search</button>-->
               
